ae763c56ce8708d4e07908f3c76b9ab6705ba47e
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2023-02-09 19:59:43 +0100

    Finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,30 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.5rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       colors: {
         "very-dark-magenta": "hsl(300, 43%, 22%)",
         "soft-pink": "hsl(333, 80%, 67%)",
         "dark-grayish-magenta": "hsl(303, 10%, 53%)",
         "light-grayish-magenta": "hsl(300, 24%, 96%)",
+      },
+      backgroundImage: {
+        "pattern-top-mobile": "url('../images/bg-pattern-top-mobile.svg')",
+        "pattern-top-desktop": "url('../images/bg-pattern-top-desktop.svg')",
+        "pattern-bottom-mobile":
+          "url('../images/bg-pattern-bottom-mobile.svg')",
+        "pattern-bottom-desktop":
+          "url('../images/bg-pattern-bottom-desktop.svg')",
       },
     },
   },
