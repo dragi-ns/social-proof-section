59c8d5d7c1aae4e90d1d9169a4d531bb2c58f26d
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2023-02-09 18:40:15 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "very-dark-magenta": "hsl(300, 43%, 22%)",
+        "soft-pink": "hsl(333, 80%, 67%)",
+        "dark-grayish-magenta": "hsl(303, 10%, 53%)",
+        "light-grayish-magenta": "hsl(300, 24%, 96%)",
+      },
+    },
+  },
+  plugins: [],
+};
